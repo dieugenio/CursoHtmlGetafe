fc14ced5d54d61ac0c58145564761e44d8d8c870
Eduardo Di Eugenio <eduardo@dieugenio.net>
2014-10-08 13:54:17 +0200

    Clase del 8-10-2014
    Javascript

---
+++ Javascript/Ejercicios JavaScript/01_Mecanismos de escape/codigo.js
@@ -0,0 +1,5 @@
+var msgString = "Estos días son festivos:\n" +
+            "\tJueves: \"Fiesta del Trabajo\"\n" +
+            "\tViernes: \"2 de Mayo\""
+
+var codigoHTML = "<p>Estos son los días que trabajo esta semana:<br>Lunes<br>Martes<br>Miércoles";
